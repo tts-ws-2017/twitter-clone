49acc8740663304d686ca3b16b9cddcdbc049c86
Balint <balintdevedu@hunglish.com>
2017-11-08 16:01:09 -0500

    11/6 class work

--- balint/dark-twitter/app/models/tweet.rb
+++ balint/dark-twitter/app/models/tweet.rb
@@ -1,3 +1,9 @@
 class Tweet < ApplicationRecord
+    validates :message, presence: true, length: {maximum: 140}
+    
     belongs_to :user
+
+    has_many :tweet_tags
+    has_many :tags, through: :tweet_tags
+
 end
